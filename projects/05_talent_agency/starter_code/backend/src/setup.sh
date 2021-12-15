5172214e96bf964059df768e294c1b2bcd845dc5
anksec <ankkna@gmail.com>
2021-12-15 12:16:52 -0500

    updates

--- projects/05_talent_agency/starter_code/backend/src/setup.sh
+++ projects/05_talent_agency/starter_code/backend/src/setup.sh
@@ -9,6 +9,6 @@ export DB_NAME='agency'
 export TEST_DB_NAME='agency_test'
 
 
-export DATABASE_URL='postgres://qcwraogzvetswa:ee8fdc056c9bc5a5e1a2287dadbc0faafe6d3ee6ec7a0c6379bf7973893157e4@ec2-18-235-45-217.compute-1.amazonaws.com:5432/dakcegl2lj9luk'
-export DATABASE_URI='postgres://qcwraogzvetswa:ee8fdc056c9bc5a5e1a2287dadbc0faafe6d3ee6ec7a0c6379bf7973893157e4@ec2-18-235-45-217.compute-1.amazonaws.com:5432/dakcegl2lj9luk'
+export DATABASE_URL='postgresql://qcwraogzvetswa:ee8fdc056c9bc5a5e1a2287dadbc0faafe6d3ee6ec7a0c6379bf7973893157e4@ec2-18-235-45-217.compute-1.amazonaws.com:5432/dakcegl2lj9luk'
+export DATABASE_URI='postgresql://qcwraogzvetswa:ee8fdc056c9bc5a5e1a2287dadbc0faafe6d3ee6ec7a0c6379bf7973893157e4@ec2-18-235-45-217.compute-1.amazonaws.com:5432/dakcegl2lj9luk'
 
